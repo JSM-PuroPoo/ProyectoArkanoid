669c4eb91ee202f75c6e2976ddfa2120e11e5ba0
nalis <nalis@Purin>
2023-10-29 18:58:03 -0500

    commit: Se agregaron nuevos autores a la clase App.

--- src/app/App.java
+++ src/app/App.java
@@ -6,7 +6,9 @@ package app;
 
 /**
  *
- * @author Juan Felipe
+ * @author Juan Felipe Eraso Navarro 0222220038
+ * @author Melissa Andrea Pizarro Duarte 0222220004
+ * @author Sofhia Alejandra Prasca Teheran 0222220014
  */
 public class App {
     public static void main(String[] args) {
