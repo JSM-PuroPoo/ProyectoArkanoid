96c3e4c45c8a80a3e6e074a664b229ea1602dfda
nalis <nalis@Purin>
2023-10-29 19:48:10 -0500

    commit: ...

--- src/app/App.java
+++ src/app/App.java
@@ -4,6 +4,8 @@ package app;
  * @author Juan Felipe Eraso Navarro 0222220038
  * @author Melissa Andrea Pizarro Duarte 0222220004
  * @author Sofhia Alejandra Prasca Teheran 0222220014
+ * 
+ * KK
  */
 public class App {
 
